f4694794cffabe8d4dc0835f85ae8c07d3ac83ab
Matheus Frej <matheusfrej@hotmail.com>
2023-03-22 10:39:58 -0300

    Keyboard working with some bugs

--- src/components/Key/index.tsx
+++ src/components/Key/index.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+import { TriesContext } from 'contexts/TriesContext'
+import { FC, useContext } from 'react'
 import * as S from './styles'
 
 interface KeyT {
@@ -8,8 +9,15 @@ interface KeyT {
 }
 
 export const Key: FC<KeyT> = ({ content, color, disabled = false }) => {
+  const { onKeyPressed } = useContext(TriesContext)
+
   return (
-    <S.Button disabled={disabled} special={content.length > 1} color={color}>
+    <S.Button
+      disabled={disabled}
+      special={content.length > 1}
+      color={color}
+      onClick={() => onKeyPressed(content)}
+    >
       {content}
     </S.Button>
   )
